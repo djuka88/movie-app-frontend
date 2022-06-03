226135a9026ed5fe0339745fe101e795c27e4bc4
Milan <milan.djukic@vivifyideas.com>
2022-06-03 11:47:27 +0200

    Added comment validation schema

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,11 +1,29 @@
 import { useEffect } from "react";
 import { useParams } from "react-router";
 import { useGetMovieQuery } from "../components/queries/movie";
+import useAuth from "../components/hooks/useAuth";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 function Movie() {
   const { id } = useParams();
+  const { user } = useAuth();
   const { isLoading, error, data: movie, refetch } = useGetMovieQuery(id);
 
+  const schema = yup.object({
+    text: yup.string().required().max(500).min(1),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  
   useEffect(() => {
     refetch();
   }, []);
@@ -58,25 +76,37 @@ function Movie() {
             <div className="infoContainer">
               <div className="details">
                 <div>
-              {movie.genres.map((genre, index) => (
-                <h2 key={index} style={h2Style}>
-                  {genre.name}
-                </h2>
-              ))}
-              <div style={{ display: "inline-flex" }}>
-                <p style={{ marginRight: "10px", color:"green"}}>
-                  Likes: {movie.likes_count}
-                </p>
-                <p style={{ marginRight: "20px", color:"red" }}>Dislikes: {movie.dislikes_count}</p>
-                Total visits: {movie.times_visited}
-              </div>
-              </div>
-              <p style={pStyle}>{movie.description}</p>
+                  {movie.genres.map((genre, index) => (
+                    <h2 key={index} style={h2Style}>
+                      {genre.name}
+                    </h2>
+                  ))}
+                  <div style={{ display: "inline-flex" }}>
+                    <p style={{ marginRight: "10px", color: "green" }}>
+                      Likes: {movie.likes_count}
+                    </p>
+                    <p style={{ marginRight: "20px", color: "red" }}>
+                      Dislikes: {movie.dislikes_count}
+                    </p>
+                    Total visits: {movie.times_visited}
+                  </div>
+                </div>
+                <p style={pStyle}>{movie.description}</p>
               </div>
               <div className="comments">
-                <textarea>
-
-                </textarea>
+                <p>{user.name}</p>
+                <form>
+                  <textarea
+                    placeholder="Post comment..."
+                    rows={10}
+                    name="text"
+                    {...register("text")}
+                  ></textarea>
+                  <button type="submit" onClick={handleSubmit} style={{ marginLeft : "10px" }}>Post comment</button>
+                </form>
+                {movie.comments.map((comment,index)=>(
+                  <p key={index}>{comment.text}</p>
+                ))}
               </div>
             </div>
           </div>
